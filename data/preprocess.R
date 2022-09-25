3123800e2a7849a1e322279f204f20fcb1e03b3c
Gerdis <gerdis_anderson@web.de>
2022-09-25 22:21:49 +0200

    corrected preprocessing error

--- data/preprocess.R
+++ data/preprocess.R
@@ -14,36 +14,34 @@ colClasses=c(rep("numeric", 11), "factor"), dec=".", sep=";", header=TRUE)
 str(red)
 table(red$quality)
 
-#resample
-
-#I'll use the SMOTE algorithm to produce a balanced dataset. 
 set.seed(123)
 
-#factor levels have to be renamed for C.per parameter of SMOTE algorithm to work
-red$quality <- factor(red$quality, levels = c(3, 4, 5, 6, 7, 8), 
+### train/test split
+
+makesplit <- initial_split(red, prop=0.8, strata="quality")
+traindata <- training(makesplit)
+testdata <- testing(makesplit)
+
+#I'll use the SMOTE algorithm to produce a balanced training dataset. 
+#factor levels have to be renamed for the C.per parameter to work
+traindata$quality <- factor(traindata$quality, levels = c(3, 4, 5, 6, 7, 8), 
                        labels = c("three", "four", "five", "six", "seven", "eight")) 
 
-res_red <- SmoteClassif(quality ~.,red, 
+res_train <- SmoteClassif(quality ~.,traindata, 
                         C.perc=list(three=(70/10), four=(70/53),
                                     five=(70/681), six=(70/638), 
                                     seven=(70/199), eight=(70/18)), 
                                     dist="HVDM") #"Euclidean" doesn't work with nominal features
 
-table(res_red$quality)
-levels(res_red$quality) <- c("1","2","3","4","5","6")
+table(res_train$quality)
+levels(res_train$quality) <- c("1","2","3","4","5","6")
 
-table(res_red$quality)
+table(res_train$quality)
 #table(res_red$quality) %>% prop.table()
-
-### train/test split
-
-makesplit <- initial_split(res_red, prop=0.8, strata="quality")
-traindata <- training(makesplit)
-testdata <- testing(makesplit)
 
 ### shuffle observations
 
-train_data <- traindata[sample(1:nrow(traindata)),]
+train_data <- res_train[sample(1:nrow(res_train)),]
 test_data <- testdata[sample(1:nrow(testdata)),]
 
 #save data
